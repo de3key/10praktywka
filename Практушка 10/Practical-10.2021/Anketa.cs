baf23a2c36f243c2ec7800419558cf987163dd2d
de3key <gnosthard@gmail.com>
2021-11-24 17:30:17 +0700

    mm

---
+++ Практушка 10/Practical-10.2021/Anketa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_10._2021
+{
+    class ycheniki
+    {
+        public string FIO;
+        public string group;
+        public int ocenka;
+
+        public override string ToString()
+        {
+            return " ФИО " + FIO + " Группа=" + group + " Оценка=" + ocenka;
+        }
+    }
+}
---
+++ Практушка 10/Practical-10.2021/Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical_10._2021
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.DialogResult == DialogResult.OK)
+            {
+                int k = 0;
+                try
+                {
+                    if (textBox2.Text == "")
+                    {
+                        textBox2.Focus();
+                        throw new Exception("Либо ты айзер либо ты айзер");
+                    }
+                    string s = textBox3.Text;
+                    k++;
+                    int kl = Convert.ToInt32(textBox4.Text);
+                    k++;
+
+
+                }
+                catch (FormatException)
+                {
+                    if (k == 0)
+                    {
+                        textBox3.Focus();
+                        MessageBox.Show("Не та группа братанчик");
+                    }
+                    else if (k == 1)
+                    {
+                        textBox4.Focus();
+                        MessageBox.Show("Оценочка");
+                    }          
+                    
+                    e.Cancel = true;
+                }
+                catch (Exception E)
+                {
+                    MessageBox.Show(E.Message);
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
